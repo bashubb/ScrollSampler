8f8968ab3b39a3cc59db3d98583731b9ed63f1ae
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-02-01 10:16:01 +0100

    UI refactor

--- ScrollSampler/ContentView.swift
+++ ScrollSampler/ContentView.swift
@@ -11,58 +11,13 @@ struct ContentView: View {
     @State private var dataModel = DataModel()
     @State private var vertical = true
     
+    let formatter: NumberFormatter
+    
     var body: some View {
         NavigationSplitView {
-            Form {
-                Button("Edge opacity") {
-                    dataModel.variants[0].opacity = 0.0
-                    dataModel.variants[2].opacity = 0.0
-                }
-                
-                Button("orientation") {
-                    vertical.toggle()
-                }
-                ForEach(dataModel.variants) { variant in
-                    @Bindable var variant = variant
-                    Section(variant.id) {
-                        
-                        Text("\(variant.opacity)")
-                        LabeledContent("Opacity") {
-                            Slider(value: $variant.opacity, in: 0...1)
-                        }
-                        LabeledContent("Scale") {
-                            Slider(value: $variant.scale, in: 0...3)
-                        }
-                        LabeledContent("xOffset") {
-                            Slider(value: $variant.xOffset, in: -1000...1000)
-                        }
-                        LabeledContent("yOffset") {
-                            Slider(value: $variant.yOffset, in: 0...1000)
-                        }
-                        LabeledContent("blur") {
-                            Slider(value: $variant.blur, in: 0...50)
-                        }
-                        LabeledContent("saturation") {
-                            Slider(value: $variant.saturation, in: 0...1)
-                        }
-                        LabeledContent("Rotation degrees") {
-                            Slider(value: $variant.degrees, in: -180...180)
-                        }
-                        LabeledContent("Rotation x axis") {
-                            Slider(value: $variant.rotationX, in: 0...1)
-                        }
-                        LabeledContent("Rotation y axis") {
-                            Slider(value: $variant.rotationY, in: 0...1)
-                        }
-                        LabeledContent("Rotation z axis") {
-                            Slider(value: $variant.rotationZ, in: 0...1)
-                        }
-                        
-                    }
-                }
-            }
-            .toolbar {
-                ToolbarItem(placement: .cancellationAction) {
+            VStack(spacing:0) {
+                HStack {
+                    
                     Button {
                         dataModel = DataModel()
                     } label: {
@@ -71,7 +26,64 @@ struct ContentView: View {
                             .padding(6)
                             .background(Color.red.opacity(0.7), in: RoundedRectangle(cornerRadius: 8))
                     }
+                    
+                    Button {
+                        vertical.toggle()
+                    } label: {
+                        Text("Change Orientation")
+                            .foregroundStyle(Color.white)
+                            .padding(6)
+                            .background(Color.green.opacity(0.8), in: RoundedRectangle(cornerRadius: 8))
+                    }
                 }
+                .frame(maxWidth: .infinity)
+                .background(Color(red: 0.949, green: 0.949, blue: 0.971))
+                
+                Form {
+                    Button ("Edge opacity") {
+                        dataModel.variants[0].opacity = 0.0
+                        dataModel.variants[2].opacity = 0.0
+                    }
+                    ForEach(dataModel.variants) { variant in
+                        @Bindable var variant = variant
+                        Section(variant.id) {
+                            
+                            Text(formatter.string(for:variant.opacity) ?? "")
+                            LabeledContent("Opacity") {
+                                Slider(value: $variant.opacity, in: 0...1)
+                            }
+                            LabeledContent("Scale") {
+                                Slider(value: $variant.scale, in: 0...3)
+                            }
+                            LabeledContent("xOffset") {
+                                Slider(value: $variant.xOffset, in: -1000...1000)
+                            }
+                            LabeledContent("yOffset") {
+                                Slider(value: $variant.yOffset, in: 0...1000)
+                            }
+                            LabeledContent("blur") {
+                                Slider(value: $variant.blur, in: 0...50)
+                            }
+                            LabeledContent("saturation") {
+                                Slider(value: $variant.saturation, in: 0...1)
+                            }
+                            LabeledContent("Rotation degrees") {
+                                Slider(value: $variant.degrees, in: -180...180)
+                            }
+                            LabeledContent("Rotation x axis") {
+                                Slider(value: $variant.rotationX, in: 0...1)
+                            }
+                            LabeledContent("Rotation y axis") {
+                                Slider(value: $variant.rotationY, in: 0...1)
+                            }
+                            LabeledContent("Rotation z axis") {
+                                Slider(value: $variant.rotationZ, in: 0...1)
+                            }
+                            
+                        }
+                    }
+                }
+                .toolbar (.hidden)
             }
         } detail: {
             if vertical {
@@ -101,8 +113,13 @@ struct ContentView: View {
                 .ignoresSafeArea()
                 
             }
-            
         }
+    }
+    
+    init() {
+        formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
         
     }
 }
